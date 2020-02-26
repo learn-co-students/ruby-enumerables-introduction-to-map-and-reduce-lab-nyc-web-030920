2f5d57c1b71a0d878ee2b288eb97322769cd72ca
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-26 22:14:30 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,60 @@
 # My Code here....
+def map_to_negativize source
+  ret = []
+  source.each do |val|
+    ret << val * -1
+  end
+  ret
+end
+
+def map_to_no_change source
+  ret = []
+  source.each do |val|
+    ret << val
+  end
+  ret
+end
+
+def map_to_double source
+  ret = []
+  source.each do |val|
+    ret << val * 2
+  end
+  ret
+end
+
+def map_to_square source
+  ret = []
+  source.each do |val|
+    ret << val * val
+  end
+  ret
+end
+
+def reduce_to_total source, start=0
+  sum = start
+  count = 0
+  while count < source.length do
+    sum += source[count]
+    count += 1
+  end
+  return sum
+end
+
+def reduce_to_all_true source
+  source.each do |val|
+    if !val
+      return false
+    end
+  end
+  true
+end
+
+def reduce_to_any_true source
+  source.each do |val|
+    if val
+      return true
+    end
+  end
+  false
+end
